b45f41029407786b910e537bc9bd0cab85cafafb
purmki <purmki456@gmail.com>
2023-09-28 21:19:17 +0300

    promisses (עבודה updated with storage

--- main.js
+++ main.js
@@ -30,26 +30,99 @@
   
 
 //חשוב ביותר להבנה על עבודה של פונקציית קול באק ופארמטר של פונקצי
+// const myButton = document.getElementById("myBtn")
+
+// function weather(userCity){
+//     fetch(`https://api.openweathermap.org/data/2.5/weather?&units=metric&q=${userCity}&appid=d407c23c84bcbce8dc544d4ae41b3daa`)
+//     .then(response=>{
+//         return response.json();
+//     })
+//     .then(data=>{
+//         console.log(data);
+//         const container = document.getElementById("container");
+//         container.innerHTML=`<h1>${data.main.temp}</h1>`
+        
+//     })
+//     .catch(error=>{
+//         console.log(error);
+//     });
+// }
+// myButton.addEventListener("click",()=>{
+//     weather(myInput.value)
+// })
+
+
+
+
 const myButton = document.getElementById("myBtn")
 
 function weather(userCity){
-    fetch(`https://api.openweathermap.org/data/2.5/weather?&units=metric&q=${userCity}&appid=d407c23c84bcbce8dc544d4ae41b3daa`)
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${userCity}&appid=d407c23c84bcbce8dc544d4ae41b3daa`)
     .then(response=>{
+        console.log(response);
         return response.json();
     })
     .then(data=>{
         console.log(data);
         const container = document.getElementById("container");
-        container.innerHTML=`<h1>${data.main.temp}</h1>`
+        container.innerHTML=`<h1>${data.city.name}</h1>`
+       data.list
+        .map(
+            (weather)=>
+        
+        container.innerHTML+=`<p>${weather.main.temp}<p>
+        <p>${weather.dt_txt}<p>
+        <p>${weather.main.humidity}<p>`
+        )
+
         
     })
     .catch(error=>{
         console.log(error);
     });
+
 }
 myButton.addEventListener("click",()=>{
     weather(myInput.value)
 })
+
+
+
+
+
+
+// let counter=0
+// const booksArray=[]
+// const logButton = document.getElementById("myBtn");
+// logButton.addEventListener("click", function() {
+//     counter++
+//     const myInput1 = document.getElementById("myInput1").value;
+//     const myInput2 = document.getElementById("myInput2").value;
+//     const myInput3 = document.getElementById("myInput3").value;
+//     const book = {
+//         title: myInput1,
+//         page: +myInput2,
+//         author: myInput3
+//     };
+//     if(counter<4){
+//         booksArray.push(book);
+//         console.log(booksArray);
+//     }
+//     else if(counter==4){
+//         localStorage.setItem("booksArray",JSON.stringify(booksArray))
+//     }
+//     else{
+//         const storageValue=localStorage.getItem("booksArray")
+//         console.log(JSON.parse(storageValue));
+//         logButton.disabled = true
+//     }
+// });
+
+
+
+
+
+
 
 
 
